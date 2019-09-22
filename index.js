1b502bf04d586b308dd194a454f52d00e29adfac
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-09-22 17:25:56 +0900

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+const config = require('config')
+
+const htdocsDir = config.get('htdocsDir')
+const fontsDir = config.get('fontsDir')
+const mbtilesPath = config.get('mbtilesPath')
+const port = config.get('port')
+const host = config.get('host')
+
